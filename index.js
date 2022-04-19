f9096c3b8def18a5cef87d8df66501638d1732a6
yoshua <yoshua.carrera24@gmail.com>
2022-04-18 22:26:02 -0400

    second commit

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const state = {
   e: "end",
   f: "forEach",
 };
-
 // w3 select option
+
+const reaction = (element) => {
+  altElemet = element === "key" ? "value" : "key";
+  newKey = document.getElementById(element).value;
+  document.getElementById(altElemet).value = newKey;
+};
 
 const render = (ele, tmp) => {
   ele.innerHTML = tmp;
@@ -15,14 +20,14 @@ const render = (ele, tmp) => {
 
 const createDropDown = () => {
   const keyele = document.getElementById("key");
-  const valele = document.getElementById("val");
-
+  const valele = document.getElementById("value");
   let keystmp = "";
   let valstmp = "";
   Object.keys(state).forEach((key) => {
     keystmp += `<option value="${key}">${key}</option>`;
     valstmp += `<option value="${key}">${state[key]}</option>`;
   });
+
   render(keyele, keystmp);
   render(valele, valstmp);
 };
